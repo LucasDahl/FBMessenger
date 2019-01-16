70acf7522f8f196a1f7d8601d115e3cb46733a0e
Lucas Dahl <LucasDahl425@yahoo.com>
2019-01-16 06:58:01 -0800

    Added 3 cells for testing, and changed the background from red to white.

--- FBMessenger/FriendsController.swift
+++ FBMessenger/FriendsController.swift
@@ -9,15 +9,58 @@
 import UIKit
 
 class FriendsController: UICollectionViewController {
+    
+    // Properties
+    private let cellId = "cellId"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the background
-        collectionView.backgroundColor = UIColor.red
+        collectionView.backgroundColor = UIColor.white
+        
+        // Register the cell class
+        collectionView.register(FriendCell.self, forCellWithReuseIdentifier: cellId)
         
     }
 
+    
+    //===============================
+    // MARK: - ColelctionView methods
+    //===============================
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+    }
 
 }
 
+
+//===================
+// MARK: - Cell class
+//===================
+
+class FriendCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews() {
+        
+        // Set the background color
+        backgroundColor = UIColor.blue
+        
+    }
+    
+}
