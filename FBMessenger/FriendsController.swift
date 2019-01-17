58e7f13e6f715e677e3fb625146926495da08253
Lucas Dahl <LucasDahl425@yahoo.com>
2019-01-16 18:24:48 -0800

    Added name, time, and message labels into the cell.

--- FBMessenger/FriendsController.swift
+++ FBMessenger/FriendsController.swift
@@ -56,6 +56,10 @@ class FriendsController: UICollectionViewController, UICollectionViewDelegateFlo
 
 class FriendCell: BaseCell {
     
+    //===================
+    // MARK: - Properties
+    //===================
+    
     // Setup profileImage
     let profileImageView: UIImageView = {
         
@@ -86,6 +90,89 @@ class FriendCell: BaseCell {
         
     }()
     
+    let nameLabel: UILabel = {
+        
+       // Create the label
+        let label = UILabel()
+        
+        // Set the font
+        label.font = UIFont.systemFont(ofSize: 18)
+        
+        // Set the text
+        label.text = "Friend Name"
+        
+        return label
+        
+    }()
+    
+    let messagelabel: UILabel = {
+       
+        // Create the label
+        let label = UILabel()
+        
+        // Set the font
+        label.font = UIFont.systemFont(ofSize: 14)
+        
+        // Set the the text
+        label.text = "Your  Friend's message and something else..."
+        label.textColor = UIColor.darkGray
+        
+        return label
+        
+    }()
+    
+    let timeLabel: UILabel = {
+       
+        // Setup the label
+        let label = UILabel()
+        
+        // Set the text
+        label.text = "12:00 pm"
+        
+        // Set the text alignment
+        label.textAlignment = .right
+        
+        // Set the font
+        label.font = UIFont.systemFont(ofSize: 16)
+        
+        return label
+        
+    }()
+
+    
+    //================
+    // MARK: - Methods
+    //================
+    
+    private func setupContainerView() {
+        
+        // Setup the container
+        let containerView = UIView()
+        
+        // Add it to the subview
+        addSubview(containerView)
+        
+        addConstraintWithFormat(format: "H:|-90-[v0]|", views: containerView)
+        addConstraintWithFormat(format: "V:[v0(50)]", views: containerView)
+        
+        // Centers the cell
+        addConstraint(NSLayoutConstraint(item: containerView, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0))
+        
+        // Add the labels to the containerView
+        containerView.addSubview(nameLabel)
+        containerView.addSubview(messagelabel)
+        containerView.addSubview(timeLabel)
+        
+        // Label constraints
+        // V - vertical, H is height "v#" is which item in the array, and the number before the [v#] is padding on the left, and after is padding on the right
+        containerView.addConstraintWithFormat(format: "H:|[v0][v1(80)]-20-|", views: nameLabel, timeLabel)
+        containerView.addConstraintWithFormat(format: "V:|[v0][v1(24)]|", views: nameLabel, messagelabel)
+        containerView.addConstraintWithFormat(format: "H:|[v0]-12-|", views: messagelabel)
+        containerView.addConstraintWithFormat(format: "V:|[v0(20)]", views: timeLabel)
+        
+    }
+    
+    
     
     override func setupViews() {
         
@@ -97,16 +184,50 @@ class FriendCell: BaseCell {
         // Set the image for the cells
         profileImageView.image = UIImage(named: "zuckprofile")
         
+        setupContainerView()
+        
         // This is needed to use the auto-layout
         profileImageView.translatesAutoresizingMaskIntoConstraints = false
         dividerLineView.translatesAutoresizingMaskIntoConstraints = false
         
         // Add the constraints
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-12-[v0(68)]", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": profileImageView]))
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-12-[v0(68)]", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": profileImageView]))
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-82-[v0]|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": dividerLineView]))
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[v0(1)]|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": dividerLineView]))
+        addConstraintWithFormat(format: "H:|-12-[v0(68)]", views: profileImageView)
+        addConstraintWithFormat(format: "V:[v0(68)]", views: profileImageView)
         
+        // This centers the profileImageView
+        addConstraint(NSLayoutConstraint(item: profileImageView, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0))
+        addConstraintWithFormat(format: "H:|-82-[v0]|", views: dividerLineView)
+        addConstraintWithFormat(format: "V:[v0(1)]|", views: dividerLineView)
+  
+        
+    }
+
+    
+} // End cell
+
+// Used to add the constraints from a method
+extension UIView {
+    
+    
+    // This is a way to add constraints
+    func  addConstraintWithFormat(format: String, views: UIView...) {
+        
+        var viewsDictionary = [String: UIView]()
+        
+        // Loop through the dictionary
+        for (index, view) in views.enumerated() {
+            
+            // Set the key
+            let key = "v\(index)"
+            
+            // Add the value to the key
+            viewsDictionary[key] = view
+            view.translatesAutoresizingMaskIntoConstraints = false
+            
+        }
+        
+        // Add the constraint
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: viewsDictionary))
         
     }
     
